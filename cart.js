f89eac707e052a85e38354b4709250aefc986f0b
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2023-05-06 21:03:26 +0900

    중복 아이템 담을 시 수량 증가, 수량 증가 감소 api

--- cart.js
+++ cart.js
@@ -26,27 +26,26 @@ db.once('open', () => {
   console.log('MongoDB connected!');
 });
 
-//---
 // 스키마 생성
 const { Schema } = mongoose;
-// const {
-//   Types: { ObjectId },
-// } = new mongoose.Schema();
 const itemSchema = new Schema({
-  name: String,
-  age: Number,
-  // idss: {
-  //   type: mongoose.Types.ObjectId,
+  name: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  quantity: {
+    type: Number,
+    default: 1,
+  },
+  // price: {
+  //   type: Number,
   //   required: true,
   // },
 });
 
 // 모델 생성
 const Item = mongoose.model('Item', itemSchema);
-//---s
-
-// JSON 파싱
-// app.use(express.json());
 
 // 요청본문파싱
 app.use(bodyParser.json());
@@ -66,22 +65,20 @@ app.get('/getItems', async (req, res) => {
 // 아이템 추가
 app.post('/PostItems', async (req, res) => {
   try {
-    // // console.log(req.body, 'req.body');
+    console.log(req.body, 'req.body');
     // const { name } = req.body;
     // // console.log(name, idss, '_id');
 
-    // // 중복된 아이템인지 확인
-    // const existingItem = await Item.findOne({ name });
-    // if (existingItem) {
-    //   return res.status(409).json({ message: '이미 존재하는 아이템입니다.' });
-    // }
+    // 중복된 장바구니 아이템 검사
+    const existingCartItem = await Item.findOne(req.body);
+    // 이미 장바구니에 있는 아이템인 경우 수량을 증가
+    if (existingCartItem) {
+      existingCartItem.quantity += 1;
+      await existingCartItem.save();
+      return res.status(200).json({ message: 'Item quantity updated in cart.' });
+    }
 
-    // // 새 아이템 생성
-    // const item = new Item({ name });
-    // await item.save();
-
-    //---
-    console.log(req, 'req.body');
+    // 새 아이템 생성
     const item = new Item(req.body);
     await item.save();
     res.send(item);
@@ -95,27 +92,63 @@ app.post('/PostItems', async (req, res) => {
 
 // 아이템 삭제
 app.delete('/deleteItems/:id', async (req, res) => {
-  // try {
-  //   const { id } = req.params;
-  //   console.log(req.params, 'req.params');
+  try {
+    console.log(req.params.id, 'delete req');
+    const data = await Item.findByIdAndDelete(req.params.id);
+    res.send(data);
+    //   const { id } = req.params;
+    //   console.log(req.params, 'req.params');
 
-  //   // 삭제할 아이템 찾기
-  //   const item = await Item.findById(id);
-  //   if (!item) {
-  //     return res.status(404).json({ message: '아이템이 존재하지 않습니다.' });
-  //   }
+    //   // 삭제할 아이템 찾기
+    //   const item = await Item.findById(id);
+    //   if (!item) {
+    //     return res.status(404).json({ message: '아이템이 존재하지 않습니다.' });
+    //   }
 
-  //   await item.remove();
+    //   await item.remove();
 
-  //   res.status(200).json({ message: '아이템이 삭제되었습니다.' });
-  // } catch (error) {
-  //   console.error(error);
-  //   res.status(500).json({ message: '서버 에러' });
-  // }
+    //   res.status(200).json({ message: '아이템이 삭제되었습니다.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: '서버 에러' });
+  }
   //---
-  console.log(req.params.id, 'delete req');
-  const data = await Item.findByIdAndDelete(req.params.id);
-  res.send(data);
+});
+
+// 아이템 수량 증가
+app.put('/increase/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const item = await Item.findById(id);
+    console.log(item, 'itme');
+    if (!item) throw new Error('Item not found');
+    item.quantity += 1;
+    await item.save();
+    res.status(200).json(item);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// 아이템 수량 감소
+app.put('/decrease/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const item = await Item.findById(id);
+    if (!item) throw new Error('Item not found');
+    if (item.quantity === 1) {
+      // 수량이 1개일 때는 삭제 처리
+      const deletedItem = await Cart.findByIdAndDelete(id);
+      if (!deletedItem) throw new Error('Item not found');
+      res.status(200).json(deletedItem);
+    } else {
+      item.quantity -= 1;
+      await item.save();
+      res.status(200).json(item);
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 // 서버 시작
